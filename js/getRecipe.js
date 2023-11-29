752ed352eeff0c9c9f5671a175395bf5820773b3
ZachGG421git config --global user.name ZachGG421git config --global user.name ZachGG421 <chubbizg@gmail.com>
2023-11-29 06:04:56 -0500

    added functioning recipe page

--- js/getRecipe.js
+++ js/getRecipe.js
@@ -9,9 +9,57 @@ console.log('Recipe ID:', recipeId);
 
 fetchChosenRecipe(recipeId)
     .then(data => {
-        console.log('Recipe details: ', data)
+        console.log('Recipe details: ', data);
+        displayRecipeDetails(data);
     })
     .catch(error => {
         console.error('Error fetching recipe details:', error);
+        
     });
+
+
+    //new
+    function displayRecipeDetails(recipe) {
+        // Get the container element where you want to display the details
+        const recipeContainer = document.getElementById('recipeContainer');
+    
+        // Display title
+        const title = document.createElement('h1');
+        title.textContent = recipe.title;
+        recipeContainer.appendChild(title);
+    
+        // Display image
+        const image = document.createElement('img');
+        image.src = recipe.image;
+        image.alt = recipe.title;
+        recipeContainer.appendChild(image);
+    
+        // Display extendedIngredients
+        const ingredientsTitle = document.createElement('h2');
+        ingredientsTitle.textContent = 'Ingredients:';
+        recipeContainer.appendChild(ingredientsTitle);
+    
+        const ingredientsList = document.createElement('ul');
+        recipe.extendedIngredients.forEach(ingredient => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `${ingredient.amount} ${ingredient.unit} ${ingredient.name}`;
+            ingredientsList.appendChild(listItem);
+        });
+        recipeContainer.appendChild(ingredientsList);
+    
+        // Display instructions
+        const instructionsTitle = document.createElement('h2');
+        instructionsTitle.textContent = 'Instructions:';
+        recipeContainer.appendChild(instructionsTitle);
+
+        const instructionsList = document.createElement('ol');
+        recipe.analyzedInstructions.forEach(instruction => {
+            instruction.steps.forEach((step, index) => {
+                const instructionItem = document.createElement('li');
+                instructionItem.innerHTML = `${step.step}`;
+                instructionsList.appendChild(instructionItem);
+            });
+        });
+        recipeContainer.appendChild(instructionsList);
+    }    
 
